bd59bdd443931e54bb56eee7169a8e7b6f27970f
j0lol <me@j0.lol>
2026-02-08 13:18:18 +0000

    Clone rust-adventure/skein/tree/template-minimal

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use bevy::{prelude::*, scene::SceneInstanceReady};
+use bevy_skein::SkeinPlugin;
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            SkeinPlugin::default(),
+        ))
+        .add_observer(
+            // log the component from the gltf spawn
+            |ready: On<SceneInstanceReady>,
+             children: Query<&Children>,
+             characters: Query<&Character>| {
+                for entity in
+                    children.iter_descendants(ready.entity)
+                {
+                    let Ok(character) =
+                        characters.get(entity)
+                    else {
+                        continue;
+                    };
+                    info!(?character);
+                }
+            },
+        )
+        .add_systems(Startup, startup)
+        .run();
+}
+
+#[derive(Component, Default, Reflect, Debug)]
+#[reflect(Component, Default)]
+#[type_path = "api"]
+struct Character {
+    name: String,
+}
+
+fn startup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(SceneRoot(asset_server.load(
+        // Change this to your exported gltf file
+        GltfAssetLabel::Scene(0).from_asset("demo.gltf"),
+    )));
+}
